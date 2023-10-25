9a4554f54826225b66cf3bbc2aaac13333435572
whimsicalwill <knipewilliam20@gmail.com>
2023-10-25 03:44:35 -0400

    Separate human and AI moves

--- npmm-app/src/App.js
+++ npmm-app/src/App.js
@@ -5,50 +5,106 @@ import init, {
     get_board,
     get_to_move,
     get_num_moves,
-    make_move,
+    game_status,
+    make_human_move,
+    make_ai_move,
 } from "./pkg/npmm.js";
+
+const DEFAULT_ROWS = 6;
+const DEFAULT_COLS = 7;
+const DEFAULT_PLAYERS = 2;
+const DEFAULT_N_IN_A_ROW = 4;
+const DEFAULT_SEARCH_DEPTH = 7;
 
 function App() {
     const [board, setBoard] = useState([]);
     const [toMove, setToMove] = useState(0);
     const [moveNum, setMoveNum] = useState(0);
+    const [status, setStatus] = useState(-2); // -2 means game is ongoing
 
     useEffect(() => {
         init().then(() => {
-            initialize_game(6, 7, 2, 4);
-            setBoard(get_board());
-            setToMove(get_to_move());
-            setMoveNum(get_num_moves());
+            handleReset();
         });
     }, []);
 
+    const handleReset = () => {
+        initialize_game(
+            DEFAULT_ROWS,
+            DEFAULT_COLS,
+            DEFAULT_PLAYERS,
+            DEFAULT_N_IN_A_ROW,
+            DEFAULT_SEARCH_DEPTH
+        );
+        updateGameState();
+    };
+
     const handleClick = (col) => {
-        make_move(col);
+        make_human_move(col);
+        updateGameState();
+    };
+
+    // A function to update the React state after each move
+    const updateGameState = () => {
         setBoard(get_board());
         setToMove(get_to_move());
         setMoveNum(get_num_moves());
+        setStatus(game_status());
     };
+
+    // Trigger the AI move if appropriate
+    useEffect(() => {
+        if (toMove === 1 && status === -2) {
+            make_ai_move();
+            updateGameState();
+        }
+    }, [toMove, status]);
+
+    let statusText;
+    switch (status) {
+        case -2:
+            statusText = "Game Ongoing";
+            break;
+        case -1:
+            statusText = "It's a Tie!";
+            break;
+        default:
+            statusText = `Player ${status + 1} Wins!`;
+            break;
+    }
 
     return (
         <div className="App">
-            <header className="App-header">
-                <div>
+            <div className="game-container">
+                <div className="board">
                     {board.map((row, rowIndex) => (
-                        <div key={rowIndex}>
+                        <div className="row" key={rowIndex}>
                             {row.map((cell, cellIndex) => (
                                 <button
+                                    className="cell"
                                     key={cellIndex}
-                                    onClick={() => handleClick(cellIndex)}
+                                    onClick={() =>
+                                        status === -2 && handleClick(cellIndex)
+                                    }
+                                    disabled={status !== -2}
                                 >
-                                    {cell === 0 ? "-" : cell === 1 ? "X" : "O"}
+                                    {cell}
                                 </button>
                             ))}
                         </div>
                     ))}
                 </div>
-                <p>Next Move: Player {toMove}</p>
-                <p>Total Moves: {moveNum}</p>
-            </header>
+                <div className="info-panel">
+                    <p>Next Move: {toMove === 0 ? "You" : "AI"}</p>
+                    <p>Total Moves: {moveNum}</p>
+                    <p>{statusText}</p>
+                    {status !== -2 && (
+                        <button className="reset-button" onClick={handleReset}>
+                            Reset Game
+                        </button>
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
--- src/lib.rs
+++ src/lib.rs
@@ -19,32 +19,22 @@ pub struct PushUpFourController {
 }
 
 impl PushUpFourController {
-    pub fn new(n_rows: usize, n_cols: usize, num_players: usize, n_in_a_row: usize) -> PushUpFourController {
+    pub fn new(n_rows: usize, n_cols: usize, num_players: usize, n_in_a_row: usize, search_depth: usize) -> PushUpFourController {
         PushUpFourController {
             game: PushUpFour::new(n_rows, n_cols, num_players, n_in_a_row),
             eval_function: RandomEvaluationFunction::new(num_players),
-            search_depth: 5,
+            search_depth,
         }
-    }
-
-    // Make a move and get the AI's move (for simplicity)
-    pub fn make_move(&mut self, col: usize) {
-        self.game.transition(col);
-
-        // AI's move logic, using minimax_move or any other method you have
-
-        let (_, ai_move) = minimax_move(&mut self.game, &self.eval_function, self.search_depth);
-        self.game.transition(ai_move);
     }
 }
 
 // Static mutable reference to store the game state
-static mut GAME_CONTROLLER: Option<PushUpFourController> = None;
+static mut CURRENT_GAME: Option<PushUpFourController> = None;
 
 #[wasm_bindgen]
-pub fn initialize_game(n_rows: usize, n_cols: usize, num_players: usize, n_in_a_row: usize) {
+pub fn initialize_game(n_rows: usize, n_cols: usize, num_players: usize, n_in_a_row: usize, search_depth: usize) {
     unsafe {
-        GAME_CONTROLLER = Some(PushUpFourController::new(n_rows, n_cols, num_players, n_in_a_row));
+        CURRENT_GAME = Some(PushUpFourController::new(n_rows, n_cols, num_players, n_in_a_row, search_depth));
     }
 }
 
@@ -52,14 +42,20 @@ pub fn initialize_game(n_rows: usize, n_cols: usize, num_players: usize, n_in_a_
 pub fn get_board() -> Array {
     let rust_board;
     unsafe {
-        rust_board = GAME_CONTROLLER.as_ref().unwrap().game.get_board().clone();
+        rust_board = CURRENT_GAME.as_ref().unwrap().game.get_board().clone();
     }
     
     let js_board = Array::new();
     for row in rust_board.iter() {
         let js_row = Array::new();
         for &cell in row.iter() {
-            js_row.push(&JsValue::from(cell));
+            let value = match cell {
+                -1 => "-",
+                0 => "X",
+                1 => "O",
+                _ => "-", // default
+            };
+            js_row.push(&JsValue::from_str(value));
         }
         js_board.push(&js_row.into());
     }
@@ -69,22 +65,39 @@ pub fn get_board() -> Array {
 #[wasm_bindgen]
 pub fn get_to_move() -> usize {
     unsafe {
-        GAME_CONTROLLER.as_ref().unwrap().game.get_to_move()
+        CURRENT_GAME.as_ref().unwrap().game.get_to_move()
     }
 }
 
 #[wasm_bindgen]
 pub fn get_num_moves() -> usize {
     unsafe {
-        GAME_CONTROLLER.as_ref().unwrap().game.get_move_num()
+        CURRENT_GAME.as_ref().unwrap().game.get_move_num()
     }
 }
 
-// Make a move and get the AI's move (for simplicity)
 #[wasm_bindgen]
-pub fn make_move(col: usize) {
+pub fn make_human_move(col: usize) {
+    let game_controller;
     unsafe {
-        let game_controller = GAME_CONTROLLER.as_mut().unwrap();
-        game_controller.make_move(col);
+        game_controller = CURRENT_GAME.as_mut().unwrap();
+    }
+    game_controller.game.transition(col);
+}
+
+#[wasm_bindgen]
+pub fn make_ai_move() {
+    let game_controller;
+    unsafe {
+        game_controller = CURRENT_GAME.as_mut().unwrap();
+    }
+    let (_, ai_move) = minimax_move(&mut game_controller.game, &game_controller.eval_function, game_controller.search_depth);
+    game_controller.game.transition(ai_move);
+}
+
+#[wasm_bindgen]
+pub fn game_status() -> i32 {
+    unsafe {
+        CURRENT_GAME.as_ref().unwrap().game.get_game_status()
     }
 }

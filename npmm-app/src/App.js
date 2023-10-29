6375cd7e51d88fbf2de0e20423dc71fa75ebf0ae
whimsicalwill <knipewilliam20@gmail.com>
2023-10-28 21:09:59 -0400

    Change React code to properly interface with wasm bindings

--- npmm-app/src/App.js
+++ npmm-app/src/App.js
@@ -1,69 +1,68 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import "./App.css";
-import init, {
-    create_game_controller,
-    make_human_move,
-    make_ai_move,
-} from "./pkg/npmm.js";
+import init, { create_game_controller } from "./pkg/npmm.js";
 
 function App() {
-    const [gameController, setGameController] = useState(null);
+    const gameControllerRef = useRef(null);
+    const [board, setBoard] = useState([]);
+    const [toMove, setToMove] = useState(0);
+    const [moveNum, setMoveNum] = useState(0);
+    const [status, setStatus] = useState(-2); // -2 means game is ongoing
+
+    // A function to update the React state after each move
+    const updateGameState = useCallback(() => {
+        setBoard(gameControllerRef.current.get_board());
+        setToMove(gameControllerRef.current.get_to_move());
+        setMoveNum(gameControllerRef.current.get_move_num());
+        setStatus(gameControllerRef.current.get_game_status());
+    }, []);
+
+    const handleReset = useCallback(() => {
+        gameControllerRef.current = create_game_controller();
+        updateGameState();
+    }, [updateGameState]);
 
     useEffect(() => {
         init().then(handleReset);
-    }, []);
-
-    const handleReset = () => {
-        setGameController(create_game_controller());
-    };
+    }, [handleReset]);
 
     const handleClick = (col) => {
-        make_human_move(gameController, col);
-        setGameController({ ...gameController });
+        gameControllerRef.current.make_human_move(col);
+        updateGameState();
     };
 
     useEffect(() => {
-        if (
-            gameController?.get_to_move() === 1 &&
-            gameController?.get_game_status() === -2
-        ) {
+        if (toMove === 1 && status === -2) {
             setTimeout(() => {
-                make_ai_move(gameController);
-                setGameController({ ...gameController });
+                gameControllerRef.current.make_ai_move();
+                updateGameState();
             }, 25);
         }
-    }, [gameController]);
+    }, [toMove, status, updateGameState]);
 
-    let statusText;
-    const status = gameController?.get_game_status();
-    switch (status) {
-        case -2:
-            statusText = "Game Ongoing";
-            break;
-        case -1:
-            statusText = "It's a Tie!";
-            break;
-        default:
-            statusText = `Player ${status + 1} Wins!`;
-            break;
-    }
+    const getStatusText = (status) => {
+        switch (status) {
+            case -2:
+                return "Game Ongoing";
+            case -1:
+                return "It's a Tie!";
+            default:
+                return `Player ${status + 1} Wins!`;
+        }
+    };
 
     return (
         <div className="App">
             <div className="game-container">
-                {/* ... */}
                 <div className="board">
-                    {gameController?.get_board().map((row, rowIndex) => (
+                    {board.map((row, rowIndex) => (
                         <div className="row" key={rowIndex}>
                             {row.map((cell, cellIndex) => (
                                 <button
                                     className="cell"
                                     key={cellIndex}
                                     onClick={() => handleClick(cellIndex)}
-                                    disabled={
-                                        status !== -2 ||
-                                        gameController?.get_to_move() !== 0
-                                    }
+                                    disabled={status !== -2 || toMove !== 0}
                                 >
                                     {cell}
                                 </button>
@@ -72,12 +71,9 @@ function App() {
                     ))}
                 </div>
                 <div className="info-panel">
-                    <p>
-                        Next Move:{" "}
-                        {gameController?.get_to_move() === 0 ? "You" : "AI"}
-                    </p>
-                    <p>Total Moves: {gameController?.get_move_num()}</p>
-                    <p>{statusText}</p>
+                    <p>Next Move: {toMove === 0 ? "You" : "AI"}</p>
+                    <p>Total Moves: {moveNum}</p>
+                    <p>{getStatusText(status)}</p>
                     {status !== -2 && (
                         <button className="reset-button" onClick={handleReset}>
                             Reset Game

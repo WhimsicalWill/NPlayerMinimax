e7e71c0fa4ba8369c33188018b259190775414dd
whimsicalwill <knipewilliam20@gmail.com>
2023-11-05 16:10:34 -0500

    Update user game

--- src/user_game.rs
+++ src/user_game.rs
@@ -1,129 +1,136 @@
-// Name: PushUpFour
+// Game Name: PushUpFour
+// InitialState: Empty board; Player 0 moves first
 // ValidMoves: Players can place chips on the bottom row of columns that aren't full.
 // TransitionFunction: Chips are placed at the bottom of the column, pushing other chips up.
 // WinCondition: There are n consecutive chips in a row, column, or diagonal.
 // TieCondition: Either more than one player wins simultaneously or the board fills up.
 
-use crate::gametraits::{ValidMoves, TransitionFunction, WinCondition, TieCondition};
 use crate::game::{Game, GameState};
+use crate::game_elements::{BoardCell, Player};
+use crate::game_spec::GameSpec;
 
-pub struct UserGameValidMoves;
-impl ValidMoves for UserGameValidMoves {
+const N_IN_A_ROW: usize = 4;
+
+pub struct UserGameSpec;
+impl GameSpec for UserGameSpec {
+    fn get_initial_board(&self, rows: usize, cols: usize) -> Vec<Vec<BoardCell>> {
+        vec![vec![None; cols]; rows]
+    }
+
+    fn get_initial_to_move(&self) -> Player {
+        Player::Player0
+    }
+
     fn get_valid_moves(&self, game: &Game) -> Vec<(usize, usize)> {
         let bottom_row = game.get_num_rows() - 1;
         (0..game.get_num_cols())
             .filter_map(|col| {
-                if game.get_state().get_board()[0][col] == -1 {
-                    Some((bottom_row, col))  // (row, col) with origin at the top left
-                } else {
-                    None
+                match game.get_board()[0][col] {
+                    None => Some((bottom_row, col)), // (row, col) with origin at the top left
+                    _ => None,
                 }
             })
             .collect()
     }
-}
 
-pub struct UserGameTransitionFunction;
-impl TransitionFunction for UserGameTransitionFunction {
     fn transition(&self, game: &Game, _move_row: usize, move_col: usize) -> GameState {
-        let mut board_copy = game.get_state().get_board().clone();
-        
+        let mut board_copy = game.get_board().clone();
+
         // Push the new chip up the bottom, shifting other chips up
         for row in 0..game.get_num_rows() - 1 {
             board_copy[row][move_col] = board_copy[row + 1][move_col];
         }
-        board_copy[game.get_num_rows() - 1][move_col] = game.get_state().get_to_move() as i32;
-        
-        GameState::new(
-            (game.get_state().get_to_move() + 1) % game.get_num_players(), 
-            game.get_state().get_move_num() + 1, 
-            board_copy
-        )
-    }
-}
+        board_copy[game.get_num_rows() - 1][move_col] = Some(game.get_to_move());
 
-pub struct UserGameWinCondition;
-impl WinCondition for UserGameWinCondition {
-    fn is_win(&self, game: &Game, player: usize) -> bool {
-        is_win(game, player)
+        // Update the player to move, the move number, and the state of the baord
+        GameState::new(game.get_next_player(), game.get_move_num() + 1, board_copy)
     }
-}
 
-pub struct UserGameTieCondition;
-impl TieCondition for UserGameTieCondition {
+    fn is_win(&self, game: &Game, player: Player) -> bool {
+        self.is_row_win(game, player) || self.is_col_win(game, player) || self.is_diag_win(game, player)
+    }
+
     fn is_tie(&self, game: &Game) -> bool {
-        game.get_state().get_move_num() == game.get_num_rows() * game.get_num_cols() || is_tie_after_transition(game)
+        game.get_move_num() == game.get_num_rows() * game.get_num_cols()
+            || self.is_tie_after_transition(game)
     }
 }
 
-pub fn is_sequence_win(sequence: &[i32], player: usize, n_in_a_row: usize) -> bool {
-    sequence.iter().filter(|&&x| x == player as i32).count() >= n_in_a_row
-}
+impl UserGameSpec {
+    fn is_sequence_win(&self, sequence: &[BoardCell], player: Player) -> bool {
+        sequence
+            .iter()
+            .filter(|&cell| *cell == Some(player))
+            .count()
+            >= N_IN_A_ROW
+    }
 
-pub fn is_row_win(game: &Game, player: usize) -> bool {
-    let num_cols = game.get_num_cols();
-    let n_in_a_row = game.get_n_in_a_row();
-
-    for row in game.get_state().get_board().iter() {
-        for i in 0..num_cols - n_in_a_row + 1 {
-            if is_sequence_win(&row[i..i+n_in_a_row], player, n_in_a_row) {
-                return true;
+    fn is_row_win(&self, game: &Game, player: Player) -> bool {
+        let num_cols = game.get_num_cols();
+        for row in game.get_state().get_board().iter() {
+            for i in 0..num_cols - N_IN_A_ROW + 1 {
+                if self.is_sequence_win(&row[i..i + N_IN_A_ROW], player) {
+                    return true;
+                }
             }
         }
+        false
     }
-    false
-}
 
-pub fn is_col_win(game: &Game, player: usize) -> bool {
-    let num_cols = game.get_num_cols();
-    let num_rows = game.get_num_rows();
-    let n_in_a_row = game.get_n_in_a_row();
-
-    for col in 0..num_cols {
-        let col_elems: Vec<i32> = game.get_state().get_board().iter().map(|row| row[col]).collect();
-        for i in 0..num_rows - n_in_a_row + 1 {
-            if is_sequence_win(&col_elems[i..i + n_in_a_row], player, n_in_a_row) {
-                return true;
+    fn is_col_win(&self, game: &Game, player: Player) -> bool {
+        let num_cols = game.get_num_cols();
+        let num_rows = game.get_num_rows();
+        for col in 0..num_cols {
+            let col_elems: Vec<BoardCell> = game
+                .get_state()
+                .get_board()
+                .iter()
+                .map(|row| row[col])
+                .collect();
+            for i in 0..num_rows - N_IN_A_ROW + 1 {
+                if self.is_sequence_win(&col_elems[i..i + N_IN_A_ROW], player) {
+                    return true;
+                }
             }
         }
+        false
     }
-    false
-}
 
-pub fn is_diag_win(game: &Game, player: usize) -> bool {
-    let num_rows = game.get_num_rows();
-    let num_cols = game.get_num_cols();
-    let n_in_a_row = game.get_n_in_a_row();
+    fn is_diag_win(&self, game: &Game, player: Player) -> bool {
+        let num_rows = game.get_num_rows();
+        let num_cols = game.get_num_cols();
 
-    // Check main diagonals
-    for i in 0..num_rows - n_in_a_row + 1 {
-        for j in 0..num_cols - n_in_a_row + 1 {
-            let diagonal: Vec<i32> = (0..n_in_a_row).map(|k| game.get_state().get_board()[i + k][j + k]).collect();
-            if is_sequence_win(&diagonal, player, n_in_a_row) {
-                return true;
+        // Check main diagonals
+        for i in 0..num_rows - N_IN_A_ROW + 1 {
+            for j in 0..num_cols - N_IN_A_ROW + 1 {
+                let diagonal: Vec<BoardCell> = (0..N_IN_A_ROW)
+                    .map(|k| game.get_state().get_board()[i + k][j + k])
+                    .collect();
+                if self.is_sequence_win(&diagonal, player) {
+                    return true;
+                }
             }
         }
-    }
 
-    // Check counter-diagonals
-    for i in 0..num_rows - n_in_a_row + 1 {
-        for j in n_in_a_row - 1..num_cols {
-            let diagonal: Vec<i32> = (0..n_in_a_row).map(|k| game.get_state().get_board()[i + k][j - k]).collect();
-            if is_sequence_win(&diagonal, player, n_in_a_row) {
-                return true;
+        // Check counter-diagonals
+        for i in 0..num_rows - N_IN_A_ROW + 1 {
+            for j in N_IN_A_ROW - 1..num_cols {
+                let diagonal: Vec<BoardCell> = (0..N_IN_A_ROW)
+                    .map(|k| game.get_state().get_board()[i + k][j - k])
+                    .collect();
+                if self.is_sequence_win(&diagonal, player) {
+                    return true;
+                }
             }
         }
+        false
     }
-    false
-}
 
-fn is_tie_after_transition(game: &Game) -> bool {
-    let num_players = game.get_num_players();
-    let winners: Vec<bool> = (0..num_players).map(|player| is_win(game, player)).collect();
-    winners.iter().filter(|&&x| x).count() > 1
-}
-
-// Note: You might need a similar `is_win` function that works with game: &Game
-pub fn is_win(game: &Game, player: usize) -> bool {
-    is_row_win(game, player) || is_col_win(game, player) || is_diag_win(game, player)
+    fn is_tie_after_transition(&self, game: &Game) -> bool {
+        let num_players = game.get_num_players();
+        let winners: Vec<bool> = (0..num_players)
+            .map(|player| self.is_win(game, Player::from(player)))
+            .collect();
+        winners.iter().filter(|&&x| x).count() > 1
+    }
 }
